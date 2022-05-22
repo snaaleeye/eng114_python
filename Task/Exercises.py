b7c384c5f48dd3205e5c0a70256ce31e79c518fa
Sharmake Naaleeye <sharmakenaaleeye@Sharmakes-MacBook-Air.local>
2022-05-22 19:11:53 +0100

    initial commit

---
+++ Task/Exercises.py
@@ -0,0 +1,82 @@
+# Create programme - "you give your age and it calculates when you were born""
+#
+# name = input("Please enter your name")
+# age = input("Please enter your age")
+#
+# print(f" Your name is {name}. You were born in {2022 - int(age)} or {2021 - int(age)} .")
+#
+# from datetime import date
+# name = input("Please enter your name")
+# age = input("Please enter your age")
+# month = input("Please enter your birth month")
+# day = input("Please enter your birth day")
+#
+# month > ["January" "February" "March" "April" "May"]
+# month < ["June" "July" "August" "September" "October" "November" "December"]
+#
+# 2022 == True
+# 2021 == False
+#
+#
+# if current_date.month < int(month):
+#     birth_year -=
+#
+# else:
+#     birth_year
+#
+# print(f" Hello Human. Your name is {name}. You were born in .")
+#
+#
+#
+# def age(name, year, month, day):
+#     print(f"Hello {name}, nice to meet you!")
+#     if month <= 5 and day <= 19:
+#         print("You were born in")
+#     elif month >= 5 and day >= 19:
+#         print("You were born in")
+#     else:
+#         print("Error with your birthday")
+#
+# name = input("Please enter your name")
+# age = input("Please enter your age")
+# month = input("Please enter your birth month")
+# day = input("Please enter your birth day")
+#
+# A = (2022 - int(year))
+# B = (2021 - int(year))
+#
+# from datetime import date
+#
+# age = int(input("What is your age?"))
+# born_year = this_year - age
+# print(f"You were born": {born_year})
+#
+# today = date.today()
+# age = today.year - birthdate.year - ((today.month, today.day)) < (birthdate.month, birthdate.day)
+#
+# TASK
+#
+# name = input("Please enter your name")
+# height = input("Please enter your height")
+# favourite_colour = input("Please enter your favourite colour")
+# secret_spirit_animal = input("Please enter your secret spirit animal")
+#
+# print(f"Hello Human! Your name is {name}. You are {height} tall. Your favourite colour is {favourite_colour}")
+#
+# print(f"Your secret animal's first letter is {(secret_spirit_animal)[0]}."
+#       f"Your secret animal's last letter is {(secret_spirit_animal)[-1]}. "
+#       f"The Secret Spirit Animal has {(len(secret_spirit_animal))} letters.")
+#
+# guess = input("What is the secret animal?").lower()
+#
+# if guess == secret_spirit_animal:
+#     print("Congratulations! You are right")
+#
+# else:
+#     print("Better luck next time")
+#
+# print(Hw[7:])
+# print(Hw[-5])
+# print(Hw[:5]) after slice
+# print(Hw[0:5]) range of slice
+# print(len(Hw)) tells us total length
---
+++ Variables.py
@@ -0,0 +1,35 @@
+#  Variable - a declaration of value that can be changed.
+#  Good for skeleton code
+#
+# a = 1 int
+# b = 2 int
+# c = 3.5 float
+#
+#
+#
+# hi = "hello world!" string
+#
+# print(type(hi))
+# print(type(a))
+# print(type(b))
+#
+# a = input("Please enter the value of a")
+# print(a)
+#
+# name = "Sharmake"
+# age = "24"
+# DOB = "10/06/1997"
+#
+# print(name)
+# print(age)
+# print(DOB)
+#
+# name = input("Please enter your name")
+# age = input("Please enter your age")
+# DOB = input("Please enter your Date of Birth")
+# height_cm = input("Please input height")
+# Address = input("Please enter where you live")
+# film = input("Please enter your favourite film")
+# food = input("Please enter your favourite food")
+#
+# print(f" Your name is {name}. You are {age} old.  You are {height_cm} tall. Your date of birth is {DOB}. Your favourite film is {film}. You live in {Address}. Your favourite food is  {food}")
