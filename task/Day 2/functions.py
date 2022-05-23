ab4d49569286bddf2f8c65629b5e5077ab865e13
Sharmake Naaleeye <sharmakenaaleeye@Sharmakes-MacBook-Air.local>
2022-05-23 12:32:25 +0100

    python update

---
+++ task/Day 2/functions.py
@@ -0,0 +1,29 @@
+# def print_something(print_string):
+#     print(print_string)
+#
+#
+# print_something("Hello World")
+
+# def greeting(name):
+#     print("Hello, my name is " + name)
+#
+# greeting("Sharmake")
+
+def addition(int1, int2):
+    return int1 + int2
+
+def subtraction(int1, int2):
+    return int1 - int2
+
+def multiplication(int1, int2):
+    return int1 * int2
+
+def division(int1, int2):
+    return int1 / int2
+
+
+
+
+
+
+
--- task/Day 2/loops.py
+++ task/Day 2/loops.py
@@ -1,8 +1,8 @@
-list_data = [1, 2, 3, 4, 5]
-embedded_list = [[1,2,3],[4,5,6]]
-dict_data = {1: {"name": "Bronson", "money": "$0.05"}, 2: {"name": "Masha","money": "$3.66"}, 3: {"name": "Roscoe","money":"$1.14"}}
-
-#for num in list_data:
+# list_data = [1, 2, 3, 4, 5]
+# embedded_list = [[1,2,3],[4,5,6]]
+# dict_data = {1: {"name": "Bronson", "money": "$0.05"}, 2: {"name": "Masha","money": "$3.66"}, 3: {"name": "Roscoe","money":"$1.14"}}
+#
+# #for num in list_data:
 #    print(num * 2)
 #    print("Hello :)")
 #    print("Testing")
